2ad17f877478aa256093caaa9da8de8a0f942f51
Stefan Schroeder <ondekoza@gmail.com>
2023-09-09 17:27:29 +0200

    installer

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 echo "Hello co2meter"
 
-sh <(curl -L https://raw.githubusercontent.com/StefanSchroeder/co2meter/main/install.sh)
-
 git clone https://github.com/StefanSchroeder/co2meter.git
 
 cd co2meter
+
 make hwconfig pre fetch install
 
 echo "Done"
